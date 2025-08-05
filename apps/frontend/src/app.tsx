39e004d6ae6d1e3a5037762933662be54b7eccd4
Kwame Opare Asiedu <kwame.opare.asiedu@zainartech.com>
2025-08-05 09:45:42 +0000

    Minor changes

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -3,10 +3,10 @@ import { Card } from "shared/components";
 export default function App() {
   return (
     <div className="p-4 space-y-2.5">
-      <p>Hello Monolith</p>
+      <p>Hello Zainar Monolith</p>
 
-      <Card className="inline-block max-w-[386px]">
-        <p>This is a card from the shared directory</p>
+      <Card className="inline-block max-w-[480px]">
+        <p>This is a card component from the shared directory</p>
       </Card>
     </div>
   );
